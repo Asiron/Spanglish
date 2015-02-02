8c8350e1a7a2e8e6e51f23339dfee22d2900a8b7
asiron91 <maciej.zurad@gmail.com>
2015-02-02 02:14:06 +0100

    Adding RelayCommand and SQLite package

---
+++ Spanglish/Helpers/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Spanglish.Helpers
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _executeAction;
+        readonly Predicate<object> _canExecuteAction;
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("RelayCommand - execute");
+
+            _executeAction = execute;
+            _canExecuteAction = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null || _canExecuteAction(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+    }
+}
--- Spanglish/Views/MainWindow.xaml.cs
+++ Spanglish/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Spanglish
+namespace Spanglish.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
