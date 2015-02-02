6dcd0f8c00096b69cb25a5b002a425ea1084aec9
asiron91 <maciej.zurad@gmail.com>
2015-02-02 02:45:01 +0100

    Adding  test project

---
+++ Spanglish/Misc/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spanglish.Misc
+{
+    public static class Constants
+    {
+        public const int MinUsernameLength = 4;
+        public const int MinLoginLength = 4;
+        public const int MinPasswordLength = 6;
+        public const int MaxWordLevel = 10;
+    }
+}
--- Spanglish/Models/Database.cs
+++ Spanglish/Models/Database.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using SQLite.Net;
+using SQLite.Net.Platform.Win32;
 
 namespace Spanglish.Models
 {
     class Database
     {
+        private string _filename;
+
+        public string Filename
+        {
+            get
+            {
+                return _filename;
+            }
+            private set
+            {
+                _filename = value;
+            }
+        }
+
+        public Database(string filename)
+        {
+            Filename = filename;
+            if (File.Exists(filename))
+                return;
+
+            using(var database = new SQLite.Net.SQLiteConnection(new SQLitePlatformWin32(), filename))
+            {
+                database.createTable<User>();
+            }
+        }
+
     }
 }
