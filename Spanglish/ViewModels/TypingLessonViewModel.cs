743a5cf04554e7d9918ece6b88dde984e1ceeeca
asiron91 <maciej.zurad@gmail.com>
2015-02-06 19:13:53 +0100

    Working, before changes to editable headers

--- Spanglish/ViewModels/TypingLessonViewModel.cs
+++ Spanglish/ViewModels/TypingLessonViewModel.cs
@@ -31,7 +31,6 @@ namespace Spanglish.ViewModels
 
         protected override bool CanAcceptCurrentWord(object p)
         {
-            Console.WriteLine("ALLA");
             return CurrentWordToEdit != null && !HasErrors;
         }
 
